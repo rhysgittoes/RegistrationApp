79f0911be8a2b280dc8b2a2f49f61bd06307eece
Rhys Gittoes <rhys@gittoes.org>
2018-04-13 13:50:09 +0800

    Routes and Database changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
+
+
+      resources :users
 
 end
