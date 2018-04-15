15ae9038760751fc9881cd4d2c775417a1673df7
Rhys Gittoes <rhys@gittoes.org>
2018-04-15 13:31:06 +0800

    Added Pry-Rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+   def index
+      @user = User.find(session[:user_id])
+      if @current_user == nil 
+         render "/pages/index"
+      else 
+         redirect_to user_path
+      end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
       resources :users
       resources :sessions
 
+      # Makes it look nice i think?
+      get 'signup', to: 'users#new', as: 'signup'
+      get 'login', to: 'sessions#new', as: 'login'
+      get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
