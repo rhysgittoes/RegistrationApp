353a92a67443b6817525428034606809a067992b
Rhys Gittoes <rhys@gittoes.org>
2018-04-16 19:30:35 +0800

    Big Javascript changes for form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,17 +3,21 @@ document.addEventListener('turbolinks:load', function(){
   $("#basic_information_button").click(function(){
 
     if ($('#location_information').is(":visible")){
-        $("#location_information").fadeToggle( "slow", "linear" ); 
+        $("#location_information").fadeToggle( "slow", "linear" );
+        $("#location_chevron_information_container").fadeToggle( "slow", "linear");  
       }
       if ($('#connection_information').is(":visible")){
-        $("#connection_information").fadeToggle( "slow", "linear" ); 
+        $("#connection_information").fadeToggle( "slow", "linear" );
+        $("#connection_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#finish_area').is(":visible")){
-        $("#finish_area").fadeToggle( "slow", "linear" ); 
+        $("#finish_area").fadeToggle( "slow", "linear" );
+        $("#finish_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
 
 
     $("#basic_information").delay(700).fadeToggle( "slow", "linear" );
+    $("#basic_chevron_information_container").fadeToggle( "slow", "linear");
    });
 
 
@@ -22,14 +26,18 @@ document.addEventListener('turbolinks:load', function(){
 
       if ($('#basic_information').is(":visible")){
         $("#basic_information").fadeToggle( "slow", "linear" ); 
+        $("#basic_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#connection_information').is(":visible")){
-        $("#connection_information").fadeToggle( "slow", "linear" ); 
+        $("#connection_information").fadeToggle( "slow", "linear" );
+        $("#connection_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#finish_area').is(":visible")){
-        $("#finish_area").fadeToggle( "slow", "linear" ); 
+        $("#finish_area").fadeToggle( "slow", "linear" );
+        $("#finish_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
-    $("#location_information").delay(700).fadeToggle( "slow", "linear" );  
+    $("#location_information").delay(700).fadeToggle( "slow", "linear" );
+    $("#location_chevron_information_container").fadeToggle(  "linear");  
 
    });
 
@@ -39,17 +47,21 @@ document.addEventListener('turbolinks:load', function(){
 
     if ($('#location_information').is(":visible")){
         $("#location_information").fadeToggle( "slow", "linear" ); 
+        $("#location_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#basic_information').is(":visible")){
-        $("#basic_information").fadeToggle( "slow", "linear" ); 
+        $("#basic_information").fadeToggle( "slow", "linear" );
+        $("#basic_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#finish_area').is(":visible")){
         $("#finish_area").fadeToggle( "slow", "linear" ); 
+        $("#finish_chevron_information_container").fadeToggle(  "linear");
       }
 
 
 
     $("#connection_information").delay(700).fadeToggle( "slow", "linear" );
+    $("#connection_chevron_information_container").fadeToggle(  "linear"); 
 
    });
 
@@ -57,17 +69,25 @@ document.addEventListener('turbolinks:load', function(){
 
     if ($('#location_information').is(":visible")){
         $("#location_information").fadeToggle( "slow", "linear" ); 
+        $("#location_chevron_information_container").fadeToggle( "slow", "linear"); 
+
+
       }
       if ($('#basic_information').is(":visible")){
-        $("#basic_information").fadeToggle( "slow", "linear" ); 
+        $("#basic_information").fadeToggle( "slow", "linear" );
+         $("#basic_chevron_information_container").fadeToggle( "slow", "linear"); 
       }
       if ($('#connection_information').is(":visible")){
-        $("#connection_information").fadeToggle( "slow", "linear" ); 
-      }
+        $("#connection_information").fadeToggle( "slow", "linear" );
+        $("#connection_chevron_information_container").fadeToggle( "linear");
 
+ 
+
+      }
 
 
     $("#finish_area").delay(700).fadeToggle( "slow", "linear" );
+    $("#finish_chevron_information_container").fadeToggle( "linear");
    });
 
 
