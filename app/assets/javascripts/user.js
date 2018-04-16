b58ecb755cc0130c31b483044cfcc1ce257097f6
Rhys Gittoes <rhys@gittoes.org>
2018-04-16 11:55:21 +0800

    JS workign nicely on registration form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,25 +1,73 @@
 document.addEventListener('turbolinks:load', function(){
  
   $("#basic_information_button").click(function(){
-    $("#basic_information").fadeToggle( "slow", "linear" );
+
+    if ($('#location_information').is(":visible")){
+        $("#location_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#connection_information').is(":visible")){
+        $("#connection_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#finish_area').is(":visible")){
+        $("#finish_area").fadeToggle( "slow", "linear" ); 
+      }
+
+
+    $("#basic_information").delay(700).fadeToggle( "slow", "linear" );
    });
 
 
 
   $("#location_information_button").click(function(){
-    $("#basic_information").fadeToggle( "slow", "linear" );  
+
+      if ($('#basic_information').is(":visible")){
+        $("#basic_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#connection_information').is(":visible")){
+        $("#connection_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#finish_area').is(":visible")){
+        $("#finish_area").fadeToggle( "slow", "linear" ); 
+      }
     $("#location_information").delay(700).fadeToggle( "slow", "linear" );  
+
    });
 
 
 
   $("#connection_information_button").click(function(){
-    // $("#location_information").css("display" == 'none');
-    $("#connection_information").fadeToggle( "slow", "linear" );
+
+    if ($('#location_information').is(":visible")){
+        $("#location_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#basic_information').is(":visible")){
+        $("#basic_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#finish_area').is(":visible")){
+        $("#finish_area").fadeToggle( "slow", "linear" ); 
+      }
+
+
+
+    $("#connection_information").delay(700).fadeToggle( "slow", "linear" );
+
    });
 
   $("#complete_area_button").click(function(){
-    $("#finish_area").fadeToggle( "slow", "linear" );
+
+    if ($('#location_information').is(":visible")){
+        $("#location_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#basic_information').is(":visible")){
+        $("#basic_information").fadeToggle( "slow", "linear" ); 
+      }
+      if ($('#connection_information').is(":visible")){
+        $("#connection_information").fadeToggle( "slow", "linear" ); 
+      }
+
+
+
+    $("#finish_area").delay(700).fadeToggle( "slow", "linear" );
    });
 
 
