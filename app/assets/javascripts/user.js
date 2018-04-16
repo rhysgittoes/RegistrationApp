012cfbd6aa465a52feb9b8cf1031c6f33a1f9ecf
Rhys Gittoes <rhys@gittoes.org>
2018-04-16 11:47:56 +0800

    Js work with registration form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,27 @@
 document.addEventListener('turbolinks:load', function(){
  
   $("#basic_information_button").click(function(){
-    $("#basic_information").fadeToggle( "slow", "linear" );  
+    $("#basic_information").fadeToggle( "slow", "linear" );
    });
+
+
+
+  $("#location_information_button").click(function(){
+    $("#basic_information").fadeToggle( "slow", "linear" );  
+    $("#location_information").delay(700).fadeToggle( "slow", "linear" );  
+   });
+
+
+
+  $("#connection_information_button").click(function(){
+    // $("#location_information").css("display" == 'none');
+    $("#connection_information").fadeToggle( "slow", "linear" );
+   });
+
+  $("#complete_area_button").click(function(){
+    $("#finish_area").fadeToggle( "slow", "linear" );
+   });
+
 
 });
 
@@ -10,6 +29,9 @@ document.addEventListener('turbolinks:load', function(){
 
 
 
+     
 
+
+ 
 
 
